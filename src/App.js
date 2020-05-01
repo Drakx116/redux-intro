cc9393570f1e994a6552db737baf7bb2d13bc04d
Drakx116 <alexandre2.guyot@gmail.com>
2020-05-01 16:54:42 +0200

    Add client interaction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import auth from './actions/auth';
 
-function App() {
+
+const App = () => {
+	const token = useSelector(state => state.auth.token);
+	const dispatch = useDispatch();
+
+	const message = (token) ? 'Welcome User #' + token : 'Welcome Guest'
+
 	return (
 		<div className="container">
 			<h1> Redux Intro </h1>
+			<b> { message } </b>
+
+			<br/>
+			<button onClick={ () => dispatch(auth.addToken("eZ5deJQ4dEH4kOQ6mXH1xGX6bQK5fXV8")) }> LOGIN </button>
+			<button onClick={ () => dispatch(auth.removeToken()) }> LOGOUT </button>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// store.subscribe(() => {
-// 	console.log(store.getState().auth.token);
-// });
-//
-// store.dispatch(authActions.addToken('S8kpFN1qkL8jJR4dAR7krB5bPC6uDW2f.sd2Vt8a3Eyv21F2zd.R21ds8D63GTu4,2fsd5s'));
-// store.dispatch(authActions.removeToken());
-
 ReactDOM.render(
 	<Provider store={ store }>
 		<App/>
